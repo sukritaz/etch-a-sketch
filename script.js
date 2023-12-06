9f5354e5a1a8cd3720ed72ddfaa5f395799a3879
sukritaz <sukri.pani@gmail.com>
2023-12-06 23:01:52 +0530

    Scroller adjusted grid

--- script.js
+++ script.js
@@ -1,34 +1,51 @@
 const drawPad = document.getElementById("draw-pad");
 
-createUI();
+document.addEventListener('DOMContentLoaded', function () {
+    createDrawPad(16);
+    const scroller = document.getElementById('scroller');
+    const selectedValue = document.getElementById('grid-resolution');
 
-function createUI() {
-    createDrawPad();
+    // Initial update
+    selectedValue.innerText = `Selected Value: ${scroller.value}`;
+
+    // Event listener for scroller change
+    scroller.addEventListener('input', function () {
+        const value = scroller.value;
+        selectedValue.innerText = `Selected Value: ${value}`;
+        while(drawPad.firstChild) {
+            drawPad.removeChild(drawPad.firstChild);
+        }
+        createDrawPad(value);
+        // You can use the 'value' variable for further processing or sending it to a server.
+    });
+});
+
+
+function createDrawPad(cellNumber) {
+    const dynamicWidth = innerWidth / 3; // Set width to half of the viewport width
+    drawPad.style.width = `${dynamicWidth}px`;
+    createDrawGrid(dynamicWidth, cellNumber)
 }
 
-function createDrawGrid(dynamicWidth) {
-    const cellNumber = 16;
-    const gridWidth = dynamicWidth/cellNumber;
-
-    for(i = 0; i<cellNumber; i++) {
+function createDrawGrid(dynamicWidth, cellNumber) {
+    const gridWidth = dynamicWidth / cellNumber;
+    for (i = 0; i < cellNumber; i++) {
         let newRow = document.createElement('div');
         newRow.className = 'row';
-        for(j = 0; j < cellNumber; j++) {
-            let newCell = document.createElement('div')
-            newCell.className = 'cell';
-            newCell.style.width = `${gridWidth}px`;
-            newCell.style.height = `${gridWidth}px`;
-            newCell.addEventListener('mouseout', ()=> {
-                newCell.style.backgroundColor = 'black';
-            });
-            newRow.appendChild(newCell);
+        for (j = 0; j < cellNumber; j++) {
+            newRow.appendChild(createCell(gridWidth));
         }
         drawPad.appendChild(newRow);
     }
 }
 
-function createDrawPad() {
-    const dynamicWidth = innerWidth / 3; // Set width to half of the viewport width
-    drawPad.style.width = `${dynamicWidth}px`;
-    createDrawGrid(dynamicWidth)
+function createCell(gridWidth) {
+    let newCell = document.createElement('div')
+    newCell.className = 'cell';
+    newCell.style.width = `${gridWidth}px`;
+    newCell.style.height = `${gridWidth}px`;
+    newCell.addEventListener('mouseout', () => {
+        newCell.style.backgroundColor = 'black';
+    });
+    return newCell;
 }
